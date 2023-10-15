907034a4d1f005cd2c5d5d8e8f78c88858e18df0
Nicolas Alancay <nico.alan.alb@gmail.com>
2023-10-15 00:36:11 -0300

    Pointcut con annotation "Pointcut".
    Podemos reutilizar el pointcut y mejora la facilidad de cambios.

--- src/main/java/com/nico/aop/aspectos/LoginAspecto.java
+++ src/main/java/com/nico/aop/aspectos/LoginAspecto.java
@@ -3,13 +3,17 @@ package com.nico.aop.aspectos;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoginAspecto {
 
-	@Before("execution(public * insertaCliente*())")
+	@Pointcut("execution(public * insertaCliente*())")
+	private void prefInsercionClienteSinArgs() {}
+
+	@Before("prefInsercionClienteSinArgs()")
 	public void antesInsertarCliente() {
 		System.out.println("El usuario está logeado");
 		System.out.println("El perfil para insertar clientes es correcto");
@@ -18,6 +22,16 @@ public class LoginAspecto {
 	@After("execution(public void com.nico.aop.dao.ClienteVipDAO.actualizaClienteVIP(com.nico.aop.Cliente, ..))")
 	public void despuesActualizarCliente() {
 		System.out.println("Perfil correcto para actualizar cliente");
+	}
+
+	@Before("prefInsercionClienteSinArgs()")
+	public void verificarRequisitosCliete() {
+		System.out.println("El cliente cumple los requisitos para ser agregado a la BBDD");
+	}
+
+	@Before("prefInsercionClienteSinArgs()")
+	public void verificarRequisitosTabla() {
+		System.out.println("Se puede insertar el cliente en la tabla");
 	}
 
 }
